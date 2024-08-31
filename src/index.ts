4388e3e45608bf1f88d7beaa9917986547c3386c
Branislav Jaksic <branislavjaksic3@gmail.com>
2024-08-31 09:52:48 +0200

    add full domain validation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import tlds from "./tlds.json";
 
 export function validateDomain(domain: string): boolean {
+  const domainPattern =
+    /^(?!www\.|http:\/\/|https:\/\/)([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+
+  const isValidDomain = domainPattern.test(domain);
+
+  if (!isValidDomain) return false;
+
   const parts = domain.split(".");
 
   if (parts.length < 2) return false;
 
   const tld = parts[parts.length - 1].toUpperCase();
+
   return tlds.includes(tld);
 }
