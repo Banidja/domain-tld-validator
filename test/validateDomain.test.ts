8f414b43a24bba09276964b0726bf20addd45890
Branislav Jaksic <branislavjaksic3@gmail.com>
2024-08-31 09:57:06 +0200

    add full domain validation

--- test/validateDomain.test.ts
+++ test/validateDomain.test.ts
@@ -5,11 +5,43 @@ describe("Domain Validator", () => {
     expect(validateDomain("google.com")).toBe(true);
   });
 
-  test("Invalid domain", () => {
+  test("Domain with valid subdomain", () => {
+    expect(validateDomain("sub.google.com")).toBe(true);
+  });
+
+  test("Domain with numeric characters", () => {
+    expect(validateDomain("123google.com")).toBe(true);
+  });
+
+  test("Domain with hyphen in valid position", () => {
+    expect(validateDomain("my-domain.com")).toBe(true);
+  });
+
+  test("Numeric second-level domain", () => {
+    expect(validateDomain("1234.com")).toBe(true);
+  });
+
+  test("Invalid TLD", () => {
     expect(validateDomain("example.invalidtld")).toBe(false);
   });
 
   test("Invalid format", () => {
     expect(validateDomain("justastring")).toBe(false);
+  });
+
+  test("Invalid domain syntax", () => {
+    expect(validateDomain("-justastring")).toBe(false);
+  });
+
+  test("Invalid space in domain", () => {
+    expect(validateDomain("goo gle.com")).toBe(false);
+  });
+
+  test("Domain with trailing period", () => {
+    expect(validateDomain("google.com.")).toBe(false);
+  });
+
+  test("Domain with underscore", () => {
+    expect(validateDomain("google_com")).toBe(false);
   });
 });
