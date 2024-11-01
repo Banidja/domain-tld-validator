afcb7c35d7e9e061b33b69c4eacada13a63fd228
Branislav Jaksic <branislavjaksic3@gmail.com>
2024-11-01 15:57:27 +0100

    fix: test failing

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,15 @@
 /** @type {import('jest').Config} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js'],
-    testMatch: ['**/test/**/*.test.ts'],
-    globals: {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+      '^.+\\.tsx?$': 'ts-jest', 
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'], 
+  testMatch: ['**/test/**/*.test.ts'],
+  globals: {
       'ts-jest': {
-        tsconfig: 'tsconfig.json',
+          tsconfig: 'tsconfig.json', 
       },
-    },
-  };
-  
+  },
+};
